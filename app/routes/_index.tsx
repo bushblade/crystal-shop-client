f7369d37fcaf099039c2550f0f274b9cce75302f
Will Adams <bushblade@gmail.com>
2025-07-25 11:34:44 +0100

    clean out app for blank slate start

---
+++ app/routes/_index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <title>Crystal Shop</title>
+      <meta name="description" content="A place to buy crystals" />
+      <h1>Crystal Shop</h1>
+    </>
+  )
+}
---
+++ app/routes/about.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <>
+      <title>About Us</title>
+      <h1>About Page</h1>
+    </>
+  )
+}
