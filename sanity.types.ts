c80c41814d02a564f85b5b3cd2979f0dd48478f9
Will Adams <bushblade@gmail.com>
2025-08-06 17:13:56 +0100

    gen new types

--- sanity.types.ts
+++ sanity.types.ts
@@ -174,10 +174,27 @@ export type PRODUCTS_QUERYResult = Array<{
   }>;
 }>;
 
+// Source: ./app/routes/products.$productSlug.tsx
+// Variable: PRODUCT_QUERY
+// Query: *[_type == "product" && slug.current == $productSlug][0] {    _id,    name,    price,    stockLevel,    description,    "images": images[]{      _key,      "alt": asset->alt,      "url": asset->url    }  }
+export type PRODUCT_QUERYResult = {
+  _id: string;
+  name: string;
+  price: number;
+  stockLevel: number;
+  description: string | null;
+  images: Array<{
+    _key: string;
+    alt: null;
+    url: string | null;
+  }>;
+} | null;
+
 // Query TypeMap
 import "@sanity/client";
 declare module "@sanity/client" {
   interface SanityQueries {
     "\n  *[_type == \"product\" && stockLevel > 0] | order(_createdAt desc) {\n    _id,\n    name,\n    price,\n    \"slug\": slug.current,\n    \"images\": images[]{\n      _key,\n      \"alt\": asset->alt,\n      \"url\": asset->url\n    }\n  }\n": PRODUCTS_QUERYResult;
+    "\n  *[_type == \"product\" && slug.current == $productSlug][0] {\n    _id,\n    name,\n    price,\n    stockLevel,\n    description,\n    \"images\": images[]{\n      _key,\n      \"alt\": asset->alt,\n      \"url\": asset->url\n    }\n  }\n": PRODUCT_QUERYResult;
   }
 }
