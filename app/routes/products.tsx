3e7c451db5de4ae34ec6dec1ec6b1379edaf59ac
Will Adams <bushblade@gmail.com>
2025-08-08 22:05:38 +0100

    show all products

---
+++ app/routes/products.tsx
@@ -0,0 +1,40 @@
+import { defineQuery } from 'groq'
+import ProductCard from '~/components/ProductCard'
+import { client } from '~/sanity/client'
+import type { Route } from './+types/_index'
+
+// We need to define the query at the top level so that sanity typegen can generate our Types correctly
+const ALL_PRODUCTS_QUERY = defineQuery(`
+  *[_type == "product" && stockLevel > 0] | order(_createdAt desc) {
+    _id,
+    name,
+    price,
+    "slug": slug.current,
+    "images": images[]{
+      _key,
+      "alt": asset->alt,
+      "url": asset->url
+    }
+  }
+`)
+
+export async function loader() {
+  return { products: await client.fetch(ALL_PRODUCTS_QUERY) }
+}
+
+export default function AllProductsPage({ loaderData }: Route.ComponentProps) {
+  const { products } = loaderData
+  return (
+    <>
+      <title>Crystal Shop</title>
+      <meta name="description" content="A place to buy crystals" />
+      <main className="max-w-7xl m-auto">
+        <div className="p-10 grid lg:grid-cols-4 md:grid-cols-2 gap-3">
+          {products.map(product => (
+            <ProductCard product={product} key={product._id} />
+          ))}
+        </div>
+      </main>
+    </>
+  )
+}
