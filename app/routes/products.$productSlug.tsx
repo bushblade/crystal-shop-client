24537b301415c5a9541f167ea02e79437314afb6
Will Adams <bushblade@gmail.com>
2025-08-16 21:49:52 +0100

    add to query, remove conditional for meta tag adjust styling

--- app/routes/products.$productSlug.tsx
+++ app/routes/products.$productSlug.tsx
@@ -10,6 +10,10 @@ const SINGLE_PRODUCT_QUERY = defineQuery(`
   *[_type == "product" && slug.current == $productSlug][0] {
     _id,
     name,
+    weight,
+    localPickup,
+    countryOfOrigin,
+    categories,
     "slug": slug.current,
     price,
     stockLevel,
@@ -41,36 +45,70 @@ export default function ProductPage({ loaderData }: Route.ComponentProps) {
   return (
     <>
       <title>{product.name}</title>
-      {product.description && (
-        <meta name="description" content={product.description} />
-      )}
+      <meta name="description" content={product.description} />
 
       <main className="max-w-7xl mx-auto p-4 md:p-8">
-        <article className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           {/* Image Section */}
           <ProductGallery images={product.images} productName={product.slug} />
 
           {/* Details Section */}
-          <section className="flex flex-col">
-            <h1 className="text-4xl font-bold mb-4">{product.name}</h1>
-            <p className="text-2xl font-semibold text-gray-800 mb-6">
-              £{product.price.toFixed(2)}
-            </p>
-            <p className="text-gray-600 mb-8 leading-relaxed">
+          <section className="flex flex-col gap-y-6">
+            <div>
+              <h1 className="text-4xl font-bold text-shadow-2sm">
+                {product.name}
+              </h1>
+              <h2 className="text-3xl font-semibold text-lime-600 mt-4 font-sans">
+                £{product.price.toFixed(2)}
+              </h2>
+            </div>
+
+            <p className="text-gray-700 leading-relaxed border-t-2 border-b-2 border-gray-500 py-8">
               {product.description}
             </p>
 
+            <h3 className="text-2xl text-gray-800">Details</h3>
+            <ul className="px-4 list-disc">
+              <li className="">
+                <span>Weight - </span>
+                {product.weight ? (
+                  <span>
+                    {product.weight >= 1000
+                      ? `${product.weight / 1000}kg`
+                      : `${product.weight}g`}
+                  </span>
+                ) : (
+                  <span>NA</span>
+                )}
+              </li>
+              <li className="">
+                <span>Local collection available? - </span>
+                <span
+                  className={`${product.localPickup ? 'text-lime-600' : 'text-red-400'}  font-semibold `}
+                >
+                  {product.localPickup ? 'Yes' : 'No'}
+                </span>
+              </li>
+              <li>
+                <span>Country of origin - </span>{' '}
+                {product.countryOfOrigin ? product.countryOfOrigin : 'NA'}
+              </li>
+            </ul>
+
             {/* Add to Cart Button */}
             <div className="mt-auto">
+              <h4 className="font-sans font-light pb-3 text-sm">
+                Note: Images shown are of the actual product you will recieve
+              </h4>
               <button
                 type="button"
-                className="w-full bg-blue-600 text-white font-bold py-3 px-6 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-colors"
+                className="shadow-2xl cursor-pointer w-full bg-indigo-600 text-white font-bold py-3 px-6 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition-colors"
               >
                 Add to Cart
               </button>
             </div>
           </section>
-        </article>
+        </div>
       </main>
     </>
   )
